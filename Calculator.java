657de4ca72851079210f484ca1a50210150e364a
Kumbhar <shraddha-karbhari.kumbhar@capgemini.com>
2019-03-05 10:25:11 +0530

    Added Calculator.java file

--- Calculator.java
+++ Calculator.java
@@ -5,4 +5,9 @@ public class Calculator
 		System.out.println("Hello world!!!");
 		System.out.println("Calculator Java Program");
 	}
+	public static void addition(int number1, int number2)
+	{
+		System.out.println("Addition =" + (number1 + number2));
+		
+	}
 }
